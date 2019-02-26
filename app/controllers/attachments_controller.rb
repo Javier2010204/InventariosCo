9e2ec226829679a14385421b308a3c7bbe63e712
Javier2010204 <2010204javiergomez@gmail.com>
2019-02-26 11:35:04 -0600

    eliminar archivos adjuntos

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,5 +1,7 @@
 class AttachmentsController < ApplicationController
 	before_action :authenticate_user!, except:[:show]
+	before_action :set_attachment, only:[:destroy]
+	before_action :set_product, only:[:destroy]
 	before_action :authenticate_owner!, except:[:show]
 
 	def new
@@ -16,12 +18,25 @@ class AttachmentsController < ApplicationController
 	end
 
 	def destroy
-		
+		@attachment.destroy
+		redirect_to @product
 	end
 
 	private
+
+		def set_attachment
+			@attachment = Attachment.find(params[:id])	
+		end
+
+		def set_product
+			@product = @attachment.product
+		end
+
 		def authenticate_owner!
-			@product = Product.find(params[:attachment][:product_id])
+
+			if params.has_key? :attachment
+				@product = Product.find(params[:attachment][:product_id])
+			end
 
 			if @product.nil? || @product.company.id != current_user.store.company.id
 				redirect_to root_path, notice: "No puedes editar este producto"
